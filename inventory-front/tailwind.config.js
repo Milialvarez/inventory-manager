a0e6adc246c706fee2a212b7fd76c8fa3d22c564
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-07-16 16:10:37 -0300

    inicio nav

--- inventory-front/tailwind.config.js
+++ inventory-front/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#A3C356',
+        primary_dark: '#6CB44C',
+        primary_darker: '#24842C',
+        secondary: '#A8AFA6',
+        secondary_dark: '#647B64',
+        secondary_darker: '#443C3C',
+        secondary_lighter: '#dbdbdbff'
+      }
+    },
   },
   plugins: [],
 };
