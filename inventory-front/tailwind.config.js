80f6603dcdda2f967292bcf0c5369e5511d484e3
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-07-24 23:00:48 -0300

    cambios en estilos de filtros

--- inventory-front/tailwind.config.js
+++ inventory-front/tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
         secondary: '#A8AFA6',
         secondary_dark: '#647B64',
         secondary_darker: '#443C3C',
-        secondary_lighter: '#dbdbdbff'
+        secondary_lighter: '#dbdbdbff',
+        background: '#e6f0cdff',
       }
     },
   },
