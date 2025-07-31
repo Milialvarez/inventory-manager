a74064d4f7fe20e6e644b269629072366ad3d1c7
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-07-31 20:59:19 -0300

    actualizacion de colores

--- inventory-front/tailwind.config.js
+++ inventory-front/tailwind.config.js
@@ -11,9 +11,9 @@ module.exports = {
         primary_darker: '#24842C',
         secondary: '#A8AFA6',
         secondary_dark: '#647B64',
-        secondary_darker: '#443C3C',
+        secondary_darker: '#363f36ff',
         secondary_lighter: '#dbdbdbff',
-        background: '#e6f0cdff',
+        background: '#e3f5bfff',
       }
     },
   },
