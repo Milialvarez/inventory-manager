b3dc4a618a1047c6145e3eed81748759c9178555
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-08-24 22:46:03 -0300

    define application entities

--- inventory-backend/src/main/java/org/inventory/inventorybackend/entities/CompositeElement.java
+++ inventory-backend/src/main/java/org/inventory/inventorybackend/entities/CompositeElement.java
@@ -1,4 +1,33 @@
 package org.inventory.inventorybackend.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CompositeElement {
+
+    @Id
+    @GeneratedValue()
+    @Column(unique = true, nullable = false, name = "id_composite_element")
+    private Long compositeElementId;
+
+    @Column
+    private String compositeElementName;
+
+    @Column
+    private String compositeElementDescription;
+
+    @Column
+    private String compositeElementType;
+
+    @OneToMany
+    @Column
+    private List<SimpleElement> simple_elements;
 }
--- inventory-backend/src/main/java/org/inventory/inventorybackend/entities/SimpleElement.java
+++ inventory-backend/src/main/java/org/inventory/inventorybackend/entities/SimpleElement.java
@@ -1,4 +1,30 @@
 package org.inventory.inventorybackend.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SimpleElement {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, name = "id_simple_element")
+    private int simpleElementId;
+
+    @Column
+    private String simpleElementName;
+
+    @Column
+    private String simpleElementDescription;
+
+    @Column
+    private Double simpleElementPrice;
 }
